72ba3a1e87406794bb6897e302ac98f434e3830f
mazixiao <1598605372@qq.com>
2018-11-22 22:21:42 +0800

    项目初始

---
+++ config.js
@@ -0,0 +1,5 @@
+//全局配置
+
+module.exports = {
+
+}
---
+++ pages/components/header/header.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '亲亲抱抱举高高',
+  },
+
+
+
+changeName:function(e){
+      this.setData({
+            motto:'啦啦啦啦'
+        })
+},
+
+
+
+})
