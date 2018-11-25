7f3f95ab9bfd3522db5d4c4b1b4d05656a82ca4c
mazixiao <1598605372@qq.com>
2018-11-25 17:34:12 +0800

    首页视频的写入

--- pages/components/index/index.js
+++ pages/components/index/index.js
@@ -13,23 +13,46 @@ Page({
     ],
     //是否显示小圆点
     indicatorDots: true,
-    other: '#fff',
-    active: '#4d7435',
-    autoplay: true,
+    //轮播图小圆点
+    swiperOther: '#fff',
+    //轮播图小圆点高亮
+    swiperActive: '#4d7435',
+    swiperAutoplay: true,
     // 自动切换时间间隔
     interval: 5000,
     // 滑动动画时长
     duration: 1000,
     // 循环轮播
-    circular: true,
+    loop: true,
+    videoSrc: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+    //视频默认隐藏
+    videoHide: true,
   },
 
 
-
+// 测试
 changeName:function(e){
   this.setData({
     navIsShow: !this.data.navIsShow
   });
+},
+
+//视频播放
+playVideo: function() {
+	this.setData({
+		videoHide: false,
+	});
+	var videoContext = wx.createVideoContext('video');
+	videoContext.play();
+},
+
+//视频关闭
+pauseViode: function() {
+	this.setData({
+		videoHide: true,
+	});
+	var videoContext = wx.createVideoContext('video');
+	videoContext.pause();
 },
 
 
