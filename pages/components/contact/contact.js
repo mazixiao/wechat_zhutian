1db8bf27f3e49ee6d8b32801c85fbb18198d57e3
mazixiao <1598605372@qq.com>
2018-12-16 21:37:48 +0800

    联系我们初始

--- pages/components/contact/contact.js
+++ pages/components/contact/contact.js
@@ -6,16 +6,28 @@ const app = getApp()
 
 Page({
   data: {
+  	tabTit: ['人才招聘', '联系我们'],
+    // 锚点 默认第一个高亮
+    anchorActive: 0,
 
+  },
+
+  // 模拟锚点
+  anchorControl:function(e) {
+    var dataId = e.currentTarget.dataset.id;
+    this.setData({
+      anchorActive: dataId,
+    });
   },
 
 
 
-changeName:function(e){
-
-},
 
 
+
+
+
+ 
 
 
 
