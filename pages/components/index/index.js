dd5725b878e8c819b7890f48ddad2ee41e0bb90a
mazixiao <1598605372@qq.com>
2019-12-01 20:44:48 +0800

    上传测试

--- pages/components/index/index.js
+++ pages/components/index/index.js
@@ -143,6 +143,151 @@ Page({
     customItem: '全部'
 
   },
+  // 上传图片
+  uploader: function () {
+
+    var that = this;
+
+    let imagesList = [];
+
+    let maxSize = 1024 * 1024;
+
+    let maxLength = 9;
+
+    let flag = true;
+
+    wx.chooseImage({
+
+      count: 9, //最多可以选择的图片总数
+
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+
+      success: function (res) {
+
+        wx.showToast({
+
+          title: '正在上传...',
+
+          icon: 'loading',
+
+          mask: true,
+
+          duration: 500
+
+        })
+
+        for (let i = 0; i < res.tempFiles.length; i++) {
+
+          if (res.tempFiles[i].size > maxSize) {
+
+            flag = false;
+
+            console.log(111)
+
+            wx.showModal({
+
+              content: '图片太大，不允许上传',
+
+              showCancel: false,
+
+              success: function (res) {
+
+                if (res.confirm) {
+
+                  console.log('用户点击确定')
+
+                }
+
+              }
+
+            });
+
+          }
+
+
+
+        }
+
+        if (res.tempFiles.length > maxLength) {
+
+          console.log('222');
+
+          wx.showModal({
+
+            content: '最多能上传' + maxLength + '张图片',
+
+            showCancel: false,
+
+            success: function (res) {
+
+              if (res.confirm) {
+
+                console.log('确定');
+
+              }
+
+            }
+
+          })
+
+        }
+
+        if (flag == true && res.tempFiles.length <= maxLength) {
+
+          that.setData({
+
+            imagesList: res.tempFilePaths
+
+          })
+
+        }
+
+        wx.uploadFile({
+
+          url: 'https://shop.gxyourui.cn',
+
+          filePath: res.tempFilePaths[0],
+
+          name: 'images',
+
+          header: {
+
+            "Content-Type": "multipart/form-data",
+
+            'Content-Type': 'application/json'
+
+          },
+
+          success: function (data) {
+
+            console.log(data, '00');
+
+          },
+
+          fail: function (data) {
+
+            console.log(data, '11');
+
+          }
+
+        })
+
+        console.log(res, '22');
+
+      },
+
+      fail: function (res) {
+
+        console.log(res);
+
+      }
+
+    })
+
+  },
+
 
 
   //tab切换
